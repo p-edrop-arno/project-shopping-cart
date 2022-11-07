0ce52c61581b4260b677388ba089efe920962bf6
P-edroP-arno <pedroparno@gmail.com>
2022-11-07 00:23:26 -0300

    Primeiros 2 requisitos de testes

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,22 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  const fetchItem = async (itemID) => {
+    const definedUrl = `https://api.mercadolibre.com/items/${itemID}`;
+  
+    try {
+      const items = await fetch(definedUrl);
+      const response = await items.json();
+      return response;
+    } catch (error) {
+      return error;
+    }
+  };
+  
+  if (typeof module !== 'undefined') {
+    module.exports = {
+      fetchItem,
+    };
+  }
+  
 });
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,22 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  const fetchProducts = async (product) => {
+    const link = `https://api.mercadolibre.com/sites/MLB/search?q=${product}`;
+  
+    try {
+      const products = await fetch(link);
+      const response = await products.json();
+      return response;
+    } catch (error) {
+      return error;
+    }
+  };
+  
+  if (typeof module !== 'undefined') {
+    module.exports = {
+      fetchProducts,
+    };
+  }
+  
 });
